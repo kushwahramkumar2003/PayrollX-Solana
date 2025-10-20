e8f57bcf22206330788031118b54aa994c4d36a2
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2025-10-20 11:44:53 +0530

    Add lint scripts to all packages for CI/CD
    - Add lint script to packages/common, packages/contracts, packages/database
    - Add eslint.config.mjs files for all packages using @payrollx/eslint-config
    - Fix import paths from @repo/eslint-config to @payrollx/eslint-config
    - Fix UI package InputProps to use type alias instead of empty interface
    - All packages now have lint scripts for CI/CD pipeline compatibility

--- packages/common/eslint.config.mjs
+++ packages/common/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@payrollx/eslint-config/react-internal";
+import { config } from "@payrollx/eslint-config/base";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
